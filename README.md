e221cc860bc99100268e256f1d16764884ade632
Hans-Jörg Schnedlitz <hans.schnedlitz@gmail.com>
2020-05-03 12:13:00 +0200

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-# Glumpat
-
-This repo contains source code for the glumpat site. 
-
-will update this eventually.
--- src/components/CardList/CardList.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const CardList = (props) => (
-  <div class="row">
-    {props.children}
-  </div>
-)
-
-export default CardList
